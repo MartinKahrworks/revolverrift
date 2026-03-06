86a378094915c4cce70a89130e2c8fac5eaa61b7
Yash <yashbondre092@gmail.com>
2026-03-06 20:20:11 +0530

    .

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,21 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', ''),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  // Gracefully handle Railway's auto-provided domain or user's PUBLIC_URL
+  let externalUrl = env('PUBLIC_URL', '');
+  if (!externalUrl && env('RAILWAY_PUBLIC_DOMAIN')) {
+    externalUrl = `https://${env('RAILWAY_PUBLIC_DOMAIN')}`;
+  } else if (externalUrl && !externalUrl.startsWith('http')) {
+    externalUrl = `https://${externalUrl}`;
+  }
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: externalUrl,
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
